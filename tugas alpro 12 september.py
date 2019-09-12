1a5f3e64cf5351fd0ff986deef8db4be719175d0
Yuan0104 <54665529+Yuan0104@users.noreply.github.com>
2019-09-12 08:50:36 +0700

    Tugas R. Yuan Abdiel Gama Bandoro-TI-42-11-1201184166
    Tugas Alpro tentang Looping, Array, if else condition

---
+++ tugas alpro 12 september.py
@@ -0,0 +1,19 @@
+print("start")
+
+GPA = [3, 2.7,2.5, 4]
+
+def perkalian():
+    Total = indeks * bonus
+    return Total
+
+bonus = 500000
+
+for indeks in GPA:
+    print(indeks)
+    if indeks > 3:
+        print("anda mendapatkan bonus = Rp.", perkalian())
+    elif indeks <= 3:
+        print("anda tidak mendapatkan bonus")
+
+
+   
